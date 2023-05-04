104391c81e49541dd2f757425bf5a41c0e96abb6
developerweb510 <124876701+luanavallejos@users.noreply.github.com>
2023-05-04 23:04:58 +0530

    changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,61 +8,76 @@ use App\Models\User;
 use App\Models\product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+
 class UserController extends Controller
 {
     public $successStatus = 200;
-        /**
-        * login api
-        *
-        * @return \Illuminate\Http\Response
-        */
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
 
 
-    public function register(Request $request){
-        // echo "<pre>";
-        // print_r($request->all());die;
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required',
             'c_password' => 'required|same:password',
         ]);
-        if ($validator->fails()){
-                return response()->json([
-                'error'=>$validator->errors()], 401);
-            }
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ], 401);
+        }
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] =  $user->createToken('threeApp')->accessToken;
         $success['name'] =  $user->name;
-        return response()->json(['success'=>$success], $this-> successStatus);
+        return response()->json(['success' => $success], $this->successStatus);
     }
 
-    public function login(){
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
             $user = Auth::user();
-            $success['token'] =  $user->createToken('threeApp')-> accessToken;
+            $success['token'] =  $user->createToken('threeApp')->accessToken;
             $success['userId'] = $user->id;
-            return response()->json(['success' => $success], $this-> successStatus);
-        }
-        else{
-            return response()->json(['error'=>'Unauthorised'], 401);
+            return response()->json(['success' => $success], $this->successStatus);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
         }
     }
 
-    public function userDetails(){
+
+    public function getCounts()
+    {
+        // $users = User::all()->except(Auth::id());
+        $users = User::count();
+        $product = product::count();
+        $publish = product::where('publish', 1)->count();
+        $active = User::where('grant', 1)->count();
+        return response()->json(['users' => $users, 'products' => $product, 'publish' => $publish, 'active' => $active]);
+    }
+
+    public function userDetails()
+    {
         $user = Auth::user();
-        return response()->json(['user' => $user], $this-> successStatus);
+        return response()->json(['user' => $user], $this->successStatus);
     }
 
-    public function logout(){
+    public function logout()
+    {
         $user = Auth::user()->token();
         $user->revoke();
         return response()->json(['message' => 'Successfully logged out']);
     }
 
-    public function addProduct(Request $request){
+    public function addProduct(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'title' => 'required',
@@ -71,26 +86,40 @@ class UserController extends Controller
             'embed_code' => 'required',
             'image' => 'required'
         ]);
+
+
         if ($validator->fails()) {
-             return response()->json(['error'=>$validator->errors()], 401);
-         }
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        // print_r($request->all());die;
+
         $product = new product();
+
+        $extension = $request->image->getClientOriginalName();
+        $filename = time() . '.' . $extension;
+        $request->image->move(public_path('images'), $filename);
+        $product->image = $filename;
+
         $product->name = $request->name;
         $product->title = $request->title;
         $product->tag = $request->tag;
         $product->description = $request->description;
         $product->embed_code = $request->embed_code;
-        $product->image = $request->image;
+        $product->image = $filename;
+        $product->publish = $request->published;
         $product->save();
-        return response()->json(['success' => $product], $this-> successStatus);
+        return response()->json(['success' => $product], $this->successStatus);
     }
 
-    public function getProducts(){
+    public function getProducts()
+    {
         $product_information = product::all();
-        return response()->json(['products' => $product_information], $this-> successStatus);
+        return response()->json(['products' => $product_information], $this->successStatus);
     }
 
-    public function deleteProducts($id){
+    public function deleteProducts($id)
+    {
         $data = product::find($id);
         $data->delete();
         return response()->json([
@@ -99,28 +128,60 @@ class UserController extends Controller
         ]);
     }
 
-    public function editProducts(Request $request, $id){
-        $name = $request['name'];
-        $title = $request['title'];
-        $tag = $request['tag'];
-        $description= $request['description'];
-        $image = $request['image'];
-        $embed_code = $request['embed_code'];
-
-        product::where('id', $id)->update(['name' => $name, 'title' => $title, 'tag' => $tag,'description' => $description, 'image' => $image, 'embed_code' => $embed_code]);
-        $updated_data=product::find($id);
-        return response()->json([
-            'Updated Products' => $updated_data,
-            'message' => 'Product Updated Successfully !!'
-        ]);
+    public function editProducts(Request $request, $id)
+    {
+        $product = product::find($id);
+        $product->name = $request['name'];
+        $product->title = $request['title'];
+        $product->tag = $request['tag'];
+        $product->description = $request['description'];
+        $product->embed_code = $request['embed_code'];
+        $product->publish = $request->published;
+        if ($request->hasFile('image')) {
+            $extension = $request->image->getClientOriginalName();
+            $filename = time() . '.' . $extension;
+            $request->image->move(public_path('images'), $filename);
+            $product->image = $filename;
+        }
+        if ($product->save())
+            return response()->json([
+                'message' => 'Product Updated Successfully !!'
+            ]);
     }
 
-    public function getUsers(){
+    public function editPublished(Request $request, $id)
+    {
+        $product = product::find($id);
+        $product->publish = $request->published;
+        if ($product->save())
+            return response()->json([
+                'message' => 'Product Updated Successfully !!'
+            ]);
+    }
+
+
+    public function editGrant(Request $request, $id)
+    {
+
+        // echo $id;
+        // die;
+        $users = User::find($id);
+
+        $users->grant = $request->grant;
+        if ($users->save())
+            return response()->json([
+                'message' => 'User Grant  Successfully !!'
+            ]);
+    }
+
+    public function getUsers()
+    {
         $users = User::all();
-        return response()->json(['All Users' => $users], 200, [], JSON_NUMERIC_CHECK);
+        return response()->json(['users' => $users], 200, [], JSON_NUMERIC_CHECK);
     }
 
-    public function deleteUser($id){
+    public function deleteUser($id)
+    {
         $data = User::find($id);
         $data->delete();
         return response()->json([
@@ -129,18 +190,18 @@ class UserController extends Controller
         ]);
     }
 
-    public function editUser(Request $request, $id){
+    public function editUser(Request $request, $id)
+    {
         $data = $request->all();
         $name = $data['name'];
         $email = $data['email'];
-        $password= bcrypt($data['password']);
+        //$password= bcrypt($data['password']);
 
-        $update = User::where('id', $id)->update(['name' => $name, 'email' => $email, 'password' => $password]);
+        $update = User::where('id', $id)->update(['name' => $name, 'email' => $email]);
 
         return response()->json([
             'message' => 'User Updated Successfully !!',
             'User' => $update,
         ]);
     }
-
 }
--- routes/api.php
+++ routes/api.php
@@ -19,17 +19,21 @@ use App\Http\Controllers\Controller;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register',[UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
+
 Route::get('user_info', [UserController::class, 'getUsers']);
-Route::post('edit-user/{id}',[UserController::class,'editUser']);
-Route::post('delete-user/{id}',[UserController::class,'deleteUser']);
+Route::post('edit-user/{id}', [UserController::class, 'editUser']);
+Route::post('edit-publish/{id}', [UserController::class, 'editPublished']);
+Route::post('edit-grant/{id}', [UserController::class, 'editGrant']);
+Route::delete('delete-user/{id}', [UserController::class, 'deleteUser']);
 Route::post('add-product', [UserController::class, 'addProduct']);
 Route::get('get-products', [UserController::class, 'getProducts']);
-Route::post('delete-products/{id}', [UserController::class, 'deleteProducts']);
+Route::get('get_counts/', [UserController::class, 'getCounts']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::delete('delete-products/{id}', [UserController::class, 'deleteProducts']);
 Route::post('edit-products/{id}', [UserController::class, 'editProducts']);
 
-Route::group(['middleware' => 'auth:api'], function(){
-Route::post('user-details', [UserController::class, 'userDetails']);
-Route::post('user-logout', [UserController::class, 'logout']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('user-logout', [UserController::class, 'logout']);
+    Route::post('user-details', [UserController::class, 'userDetails']);
 });
