68f9c13c2f149a3ec757d6249c39d9aebd67755f
Gurpreet Kaur <gurpreetkaur@codenomad.net>
2023-06-21 06:00:38 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('user_info', [UserController::class, 'getUsers']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::delete('delete-user/{id}', [UserController::class, 'deleteUser']);
 Route::post('edit-user/{id}', [UserController::class, 'editUser']);
 Route::post('edit-publish/{id}', [UserController::class, 'editPublished']);
 Route::post('edit-grant/{id}', [UserController::class, 'editGrant']);
-Route::delete('delete-user/{id}', [UserController::class, 'deleteUser']);
 Route::post('add-product', [UserController::class, 'addProduct']);
 Route::get('get-products/{galleryType}', [UserController::class, 'getProducts']);
 Route::get('get_counts/', [UserController::class, 'getCounts']);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
 Route::post('/csv_file', [UserController::class, 'readCsv']);
 Route::delete('delete-products/{id}', [UserController::class, 'deleteProducts']);
 Route::post('edit-products/{id}', [UserController::class, 'editProducts']);
@@ -37,6 +36,7 @@ Route::get('get_users_statisticks', [UserController::class, 'get_users_statistic
 Route::post('forget-password', [UserController::class, 'forget_password']);
 
 Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('user_info', [UserController::class, 'getUsers']);
     Route::post('user-logout', [UserController::class, 'logout']);
-    Route::post('user-details', [UserController::class, 'userDetails']);
+    Route::get('user-details', [UserController::class, 'userDetails']);
 });
